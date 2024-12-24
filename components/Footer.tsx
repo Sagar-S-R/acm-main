48e624a5691b641efb63116ad8a3ed81716c8ae5
Sagar <sagarsr2006@gmail.com>
2024-12-24 11:20:34 +0530

    ''

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ const Footer = () => {
         <div className="flex flex-col gap-7">
           <h5>Follow us</h5>
           <div className='flex gap-3 flex-col p-1'>
-            <Link className='flex gap-3' href={"https://tr.ee/fElHithqSm"} ><span><FaWhatsapp/> </span>WhatsApp</Link>
+            <Link className='flex gap-3' href={"https://tr.ee/fElHithqSm"} target='_blank' ><span><FaWhatsapp/> </span>WhatsApp</Link>
             <Link className='flex gap-3' href={"https://www.instagram.com/acm__rit?igsh=MXZpZHJsYXpjdXQ5aw=="} target='_blank' ><span><FaInstagram/></span> Instagram</Link>
             <Link className='flex gap-3' href={"https://www.linkedin.com/in/rit-acm-student-chapter-522bb7280"} target='_blank'><span><FaLinkedin/></span> LinkedIn</Link>
           </div>
