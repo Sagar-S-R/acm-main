8bd6c55cf327f2f8bb8b84491296319aa9f4bd32
Sagar <sagarsr2006@gmail.com>
2024-12-30 17:54:39 +0530

    "logo"

--- components/AboutIntro.tsx
+++ components/AboutIntro.tsx
@@ -45,8 +45,8 @@ const AboutIntro = () => {
                     </div>
                     <Image className="rounded-md"
                         src="/background.jpg"
-                        width={600}
-                        height={400}
+                        width={400}
+                        height={200}
                         alt="Picture of the author"
                     />
                 </div>
